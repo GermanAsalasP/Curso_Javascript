7172472e3fa9220491873e3db372fc3831c3273d
GermanAsalasP <gsgrafico@gmail.com>
2024-03-15 15:13:15 -0500

    Creacion del proyecto
    Carpetas contenedoras del proyecto

---
+++ Proyecto/Js/main.js
@@ -0,0 +1,10 @@
+function Peliculas(nombre, año, director, categoria, rolPrincipal){
+    this.nombre = nombre;
+    this.año = año;
+    this.director = director;
+    this.categoria = categoria;
+    this.rolrPrincipal = rolPrincipal
+}
+
+nombrePelicula = prompt("ingresa el nombre de la pelicula")
+
